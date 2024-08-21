fe6f70d9c48dedd25e2a3e4714398767a8591573
Quinten <56535364+qBitComputer@users.noreply.github.com>
2024-08-21 17:01:15 +0200

    Made the system, only tags need some work

--- src/main/java/me/qbitcomputer/commands/Ticket.java
+++ src/main/java/me/qbitcomputer/commands/Ticket.java
@@ -20,7 +20,7 @@ public class Ticket extends ListenerAdapter {
         if (event.getName().equals("ticket")) {
             Dotenv dotenv = Dotenv.load();
             String Ticketcategory = dotenv.get("TICKETCATEGORY");
-            String Ticketname = "ticket-" +event.getUser().getName();
+            String Ticketname = "ticket-" + event.getUser().getName();
             List tickets = event.getGuild().getTextChannelsByName(Ticketname, true);
             if (!tickets.isEmpty()) {
                 event.reply("You already have an open ticket!").setEphemeral(true).queue();
@@ -32,23 +32,75 @@ public class Ticket extends ListenerAdapter {
                 String name = dotenv.get("SERVERNAME");
                 embed.setFooter(name);
                 embed.addField("Hi there, We are ready to help!", "Please specify your type of issue and a staff member will be with you shortly!", false);
-                event.replyEmbeds(embed.build()).addActionRow(StringSelectMenu.create("choose-food")
-                        .addOption("testtest", "test1", "testing") // SelectOption with only the label, value, and description
-                        .addOptions(SelectOption.of("test2", "testing2")) // another way to create a SelectOption) // while also being the default option
-                                .build()).setEphemeral(true).queue();
-                }
+                event.replyEmbeds(embed.build()).addActionRow(StringSelectMenu.create("choose-ticket")
+                        .addOption("Billing, Ranks & Cosmetics " + Emoji.fromUnicode("U+1F4B8").getFormatted(), "billing")
+                        .addOption("Glitches, Hacking and Abuse " + Emoji.fromUnicode("U+26D4").getFormatted(), "glitch")
+                        .addOption("Events & Giveaways " + Emoji.fromUnicode("U+1F389").getFormatted(), "event")
+                        .addOption("Other issues " + Emoji.fromUnicode("U+2753").getFormatted(), "other")
+                        .build()).setEphemeral(true).queue();
             }
         }
+    }
+
     @Override
     public void onStringSelectInteraction(StringSelectInteractionEvent event) {
         Dotenv dotenv = Dotenv.load();
-        if (event.getComponentId().equals("choose-food")) {
+        if (event.getComponentId().equals("choose-ticket")) {
             String Ticketcategory = dotenv.get("TICKETCATEGORY");
-            String Ticketname = "testing-" +event.getUser().getName();
-            event.reply("a ticket will be created with issue related to: " + event.getValues().get(0)).setEphemeral(true).queue();
+            String Ticketname = "ticket-" + event.getUser().getName();
             event.getGuild().getCategoryById(Ticketcategory).createTextChannel(Ticketname).queue(channel -> {
                 channel.upsertPermissionOverride(event.getMember()).grant(Permission.VIEW_CHANNEL).queue();
+                event.reply("Ticket has been created: <#" + channel.getId() + ">").setEphemeral(true).queue();
+                if (event.getValues().get(0).equals("billing")) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("Billing issue " + event.getUser().getName());
+                    embed.setColor(Color.decode("#0013ca"));
+                    String name = dotenv.get("SERVERNAME");
+                    embed.setFooter(name);
+                    embed.addField("Hi there, We're ready to help! ", "A staff member will be with you shortly!", false);
+                    channel.sendMessageEmbeds(embed.build())
+                            .addActionRow(
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.danger("close", "Close").withEmoji(Emoji.fromUnicode("U+274C"))
+                            )
+
+                            .queue();
+                } else if (event.getValues().get(0).equals("event")) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("Event issue " + event.getUser().getName());
+                    embed.setColor(Color.decode("#0013ca"));
+                    String name = dotenv.get("SERVERNAME");
+                    embed.setFooter(name);
+                    embed.addField("Hi there, We're ready to help! ", "A staff member will be with you shortly!", false);
+                    channel.sendMessageEmbeds(embed.build())
+                            .addActionRow(
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.danger("close", "Close").withEmoji(Emoji.fromUnicode("U+274C"))
+                            )
+
+                            .queue();
+                } else if (event.getValues().get(0).equals("other")) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setTitle("Other issue " + event.getUser().getName());
+                    embed.setColor(Color.decode("#0013ca"));
+                    String name = dotenv.get("SERVERNAME");
+                    embed.setFooter(name);
+
+                    channel.sendMessageEmbeds(embed.build())
+                            .addActionRow(
+                                    net.dv8tion.jda.api.interactions.components.buttons.Button.danger("close", "Close").withEmoji(Emoji.fromUnicode("U+274C"))
+                            )
+
+                            .queue();
+                }
             });
+
+        }
+        ;
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        if (event.getComponentId().equals("close")) {
+            event.getChannel().delete().queue();
         }
     }
 }
